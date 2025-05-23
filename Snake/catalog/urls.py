e2fcb02e98945ae209859eac19a56b7ca300b028
enot2107 <mr.yntenot@mail.ru>
2025-05-23 15:58:51 +0300

    style

--- Snake/catalog/urls.py
+++ Snake/catalog/urls.py
@@ -4,7 +4,7 @@ from catalog import views
 app_name = 'catalog'
 urlpatterns = [
     path('', views.return_all_snakes, name='return_all_snakes'),
-    path('one/', views.return_one_snake, name='return_one_snake'),
+    path('one/<int:id>/', views.return_one_snake, name='return_one_snake'),
     path('poison/', views.sort_snakes_poison, name='sort_snakes_poison'),
     path('notpoison/', views.sort_snakes_not_poison, name='sort_snakes_not_poison')
 ]
